dcdee55ddcbb684493ac09ba93fba865a83bc1f5
Rok Zitko <rok.zitko@ijs.si>
2024-05-13 19:25:31 +0200

    parser for integers

---
+++ int_parser/test1.cc
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <sstream>
+#include <iomanip>
+
+using namespace std::string_literals;
+
+// Parser supporting decimal, hex, octal and binary input.
+
+uint32_t parse_uint32_t(std::string s)
+{
+   if (s.substr(0, 2) == "0b"s) {
+      return strtoul(s.substr(2).c_str(), 0, 2);
+   } else {
+      std::stringstream ss(s);
+      ss >> std::setbase(0);
+      uint32_t i;
+      ss >> i;
+      return i;
+   }
+}
+
+std::string dump(uint32_t a, std::string sep = "=")
+{
+   std::stringstream s;
+   s << std::dec << a << sep << "0x" << std::hex << a << sep << "0b" << std::bitset<32>(a);
+   return s.str();
+}
+
+uint64_t parse_uint64_t(std::string s)
+{
+   if (s.substr(0, 2) == "0b"s) {
+      return strtoull(s.substr(2).c_str(), 0, 2);
+   } else {
+      std::stringstream ss(s);
+      ss >> std::setbase(0);
+      uint64_t i;
+      ss >> i;
+      return i;
+   }
+}
+
+std::string dump(uint64_t a, std::string sep = "=")
+{
+   std::stringstream s;
+   s << std::dec << a << sep << "0x" << std::hex << a << sep << "0b" << std::bitset<64>(a);
+   return s.str();
+}
+
+int main()
+{
+     {
+	std::string a("33");
+	auto aa = parse_uint32_t(a);
+	std::cout << std::dec << aa << " " << a << std::endl;
+     }
+     {
+	std::string a("0x33");
+	auto aa = parse_uint32_t(a);
+	std::cout << std::dec << aa << " " << a << " " << std::hex << aa << std::endl;
+     }
+     {
+	std::string a("033");
+	auto aa = parse_uint32_t(a);
+	std::cout << std::dec << aa << " " << a << " " << std::oct << aa << std::endl;
+     }
+     {
+	std::string a("0b1111");
+	auto aa = parse_uint32_t(a);
+	std::cout << std::dec << aa << " " << a << " " << std::bitset<32>(aa) << " " << dump(aa) << std::endl;
+     }
+     {
+	std::string a("33");
+	auto aa = parse_uint64_t(a);
+	std::cout << std::dec << aa << " " << a << std::endl;
+     }
+     {
+	std::string a("0x33");
+	auto aa = parse_uint64_t(a);
+	std::cout << std::dec << aa << " " << a << " " << std::hex << aa << std::endl;
+     }
+     {
+	std::string a("033");
+	auto aa = parse_uint64_t(a);
+	std::cout << std::dec << aa << " " << a << " " << std::oct << aa << std::endl;
+     }
+     {
+	std::string a("0b1111");
+	auto aa = parse_uint64_t(a);
+	std::cout << std::dec << aa << " " << a << " " << std::bitset<64>(aa) << " " << dump(aa) << std::endl;
+     }
+}
