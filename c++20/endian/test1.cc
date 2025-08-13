fc0baa489b28b6321d7c2baaf0f4e1976c7d27d3
Rok Zitko <rok.zitko@ijs.si>
2025-08-13 17:08:01 +0200

    endianness test

---
+++ c++20/endian/test1.cc
@@ -0,0 +1,15 @@
+// https://www.modernescpp.com/index.php/bit-manipulation-with-c-20/
+
+#include <bit>
+#include <iostream>
+
+int main() {
+    if constexpr (std::endian::native == std::endian::big) {
+        std::cout << "big-endian" << '\n';
+    }
+    else if constexpr (std::endian::native == std::endian::little) {
+        std::cout << "little-endian"  << '\n';
+    }
+}
+
+
